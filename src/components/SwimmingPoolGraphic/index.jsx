7f0eee256c148ae659fdbc3548ff4a203e532b1f
Kia Farhang <kfarhang0@gmail.com>
2017-09-04 09:17:37 -0500

    Built container component for swimming pool graphic

---
+++ src/components/SwimmingPoolGraphic/index.jsx
@@ -0,0 +1,94 @@
+// @flow
+
+import React, { Component } from 'react';
+
+import SwimmingPool from '../SwimmingPool';
+import SwipeContainer from '../SwipeContainer';
+
+import './SwimmingPoolGraphic.css';
+
+export default class SwimmingPoolGraphic extends Component{
+
+	state = {
+		fields: [
+			{
+				name: 'Permian Basin',
+				avgWater: 10496987.86
+			},
+			{
+				name: 'Eagle Ford',
+				avgWater: 9733680.749
+			},
+			{
+				name: 'Barnett Shale',
+				avgWater: 1846268
+			}
+
+		],
+		currentFieldIndex: 2
+	}
+
+	calculateNumberOfPools = (basin: Object) =>{
+		//There are 660,000 gallons of water in a standard Olympic swimming pool
+		// https://en.wikipedia.org/wiki/Olympic-size_swimming_pool
+		return basin.avgWater / 660000;
+	}
+
+	generatePoolComponents = (num: number) =>{
+		const numberOfFullPools = Math.floor(num);
+		const finalPoolFillLevel = num - numberOfFullPools;
+
+		let pools = Array(numberOfFullPools).fill(0).map((el, index)=>{
+			return <SwimmingPool fillPercent={100} key={index}/>
+		});
+
+		pools.push(<SwimmingPool fillPercent={finalPoolFillLevel} key={Math.random()}/>);
+
+		return pools;
+	}
+
+	updateIndex = (newIndex: number) =>{
+		if (newIndex > this.state.fields.length - 1){
+			this.setState(() => ({
+	  			currentFieldIndex: 0
+			}));  
+		}else if (newIndex < 0){
+			this.setState(() => ({
+	  			currentFieldIndex: this.state.fields.length -1
+			}));  
+		}else{
+			this.setState(() => ({
+	  			currentFieldIndex: newIndex
+			}));  
+		}
+	}
+
+	moveForward = () =>{
+		this.updateIndex(this.state.currentFieldIndex+1);
+	}
+
+	moveBackward = ()=>{
+		this.updateIndex(this.state.currentFieldIndex-1);
+	}
+
+
+	render(){
+
+		let currentCounty = this.state.fields[this.state.currentFieldIndex];
+
+		const numberOfPools = this.calculateNumberOfPools(currentCounty);
+
+		const pools = this.generatePoolComponents(numberOfPools);
+
+		return(
+			<SwipeContainer swipeLeftFunction={this.moveBackward} swipeRightFunction={this.moveForward}>
+				<h3>{currentCounty.name}</h3>
+				<p>The average frac job used {currentCounty.avgWater} gallons of water.</p>
+				<p>That's enough to fill {numberOfPools.toFixed(2)} Olympic swimming pools.</p>
+				<div className='PoolContainer'>
+					{pools}
+				</div>
+			</SwipeContainer>
+		)
+	}
+}
