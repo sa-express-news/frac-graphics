34301b6e85b8460f6d2fdb0e1a101f9e83ca6fa9
Kia Farhang <kfarhang0@gmail.com>
2017-09-19 10:00:39 -0500

    Minor formatting tweaks to swimming pool component

--- src/components/SwimmingPool/index.jsx
+++ src/components/SwimmingPool/index.jsx
@@ -6,21 +6,21 @@ import WaterLevel from '../WaterLevel';
 
 import './SwimmingPool.css';
 
-export default class SwimmingPool extends Component{
+export default class SwimmingPool extends Component {
 	props: {
 		fillPercent: number,
 	};
 
 	static defaultProps = {
-    	fillPercent: 0,
-  	};
-  	
+		fillPercent: 0,
+	};
 
-	render(){
 
-		return(
+	render() {
+
+		return (
 			<div className='SwimmingPool'>
-				<WaterLevel height={this.props.fillPercent}/>
+				<WaterLevel height={this.props.fillPercent} />
 			</div>
 
 		)
